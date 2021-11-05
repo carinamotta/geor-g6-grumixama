d2021874e41fc93109ff1328c41638c199a36426
carinamotta <carinaisabellamotta@gmail.com>
2021-11-05 20:19:07 -0300

    finalized script figures and readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# geor-g6-grumixama
+---
+editor_options: 
+  markdown: 
+    wrap: 72
+---
+
+# Análise das lacunas espaciais e temporais do conjunto de dados do Atlantic Bird Traits
+
+#### [Introdução ao uso de dados geoespaciais no R](https://mauriciovancine.github.io/course-geospatial-data-r/)
+
+#### geor-g6-grumixama
+
+Carina Isabella Motta <br> Luca Gomes Nunes <br> Otto Neves
+
+------------------------------------------------------------------------
+
+#### **Objectivo**
+
+Usar o conjunto do dados do [Atlantic Bird
+Traits](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecy.2647)
+para análisar as lacunas espaciais e temporais, e determinar quais
+regiões estão faltando dados.
+
+<br>
+
+#### **Hipóteses**
+
+**H0** -> Os dados disponibilizados pelo Atlantic Bird Traits **não
+apresenta** lacunas espaciais ou temporais;
+
+**H1**->Os dados disponibilizados pelo Atlantic Bird Traits
+**apresenta** lacunas espaciais ou temporais.
+
+------------------------------------------------------------------------
+
+#### **Artigos Relevantes**
+
+[**Seven Shortfalls that Beset Large-Scale Knowledge of
+Biodiversity**](https://www.annualreviews.org/doi/10.1146/annurev-ecolsys-112414-054400)
+
+[**Evaluating shortfalls and spatial accuracy of biodiversity
+documentation in the Atlantic Forest, the most diverse and threatened
+Brazilian phytogeographic
+domain**](https://onlinelibrary.wiley.com/doi/10.1002/tax.12239)
+
+[**Knowledge gaps hamper understanding the relationship between
+fragmentation and biodiversity loss: the case of Atlantic Forest
+fruit-feeding butterflies**](https://peerj.com/articles/11673/)
--- scripts/grumixama_script.R
+++ scripts/grumixama_script.R
@@ -4,164 +4,400 @@
 #' date: 2021-11-01
 #' ---
 
+##1 CARREGAR PACOTES------------------------------------------------------------
+
+#facilitar a carregar pacotes 
 package.list <- c("here", 
                   "tidyverse",
                   "sf",
                   "geobr",
                   "raster",
-                  "spatstat")
+                  "spatstat",
+                  "ggplot2",
+                  "ggspatial",
+                  "psych")
 
-#installing the packages if they aren't already on the computer
+#instalar pacotes novos (se ainda não estiver presente)
 new.packages <- package.list[!(package.list %in% installed.packages()
                                [,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-#and loading the packages into R with a for loop
+#carregar pacotes
 for(i in package.list){library(i, character.only = T)}
- 
-#load data 
+
+
+#2 CARREGAR E LIMPAR DADOS------------------------------------------------------
+
+#carregar dados 
 birds <- readr::read_csv(here::here("dados", "birds.csv"),
-                      locale = readr::locale(encoding = "latin1"))
+                         locale = readr::locale(encoding = "latin1"), 
+                         na = c("Unknown", "NA"))
 
-#visualize data
-birds
+#excluir linhas sem dados sobre localização
+birds <- tidyr::drop_na(data = birds,
+                        any_of("Latitude_decimal_degrees"))
 
-#check class and define as tibble
-class(birds)
-as_tibble(birds)
+#confirmar se foi excluido 
+any(is.na(birds$Latitude_decimal_degrees))
+any(is.na(birds$Longitude_decimal_degrees))
 
-#select columns to construct simplified dataset
-colnames(birds)
+## 3 CALCULAR NUMERO DOS DADOS DE TRAITS CADA INDIVIDUAL----
+#criar matrix binario (tem dados = 1, sem dados = 0) e somar 
+#cada linha (individual)
 
-#separando os dados
-dados<-tidyr::separate_rows(data = birds,sep = ",")
-dados_sep <- dados[,c("Genus","Species","Binomial",
-                            "Latitude_decimal_degrees",
-                            "Longitude_decimal_degrees",
-                            "Locality","Year")]
+#separar dados imporantes em um arquivo novo
+#usamos traits numéricos que foram usado para criar a planilha 
+#final (ATLANTIC_BIRD_TRAITS_Spp_Info.csv)
+traits <- birds[,c(1, 10:32)]
 
-#replace NAs with unknown (character columns) and 0 (numeric column, Year)
-any(is.na(dados_sep))
-dados_unknown <-tidyr::replace_na(data = dados_sep, list(Genus = "Unknown",
-                                                    Species = "Unknown", 
-                                                    Binomial = "Unknown", 
-                                                    Nm_municip = "Unknown", 
-                                                    Locality = "Unknown", 
-                                                    Year = 0))
+#substituir dados existentes com 1
+traits <- traits %>% 
+  mutate_if(is.numeric, ~1 * (. != 0))
 
-#drop rows with remaining NAs (only exist in Lat Long)
-any(is.na(dados_unknown))
-dados_NA <- tidyr::drop_na(dados_unknown)
-any(is.na(dados_NA))
+#substituir dados NA com 0
+traits[is.na(traits)] = 0
 
-#coerce columns as.numeric
-summary(dados_NA)
-any(is.na(dados_NA))
-as.numeric(dados_NA$Latitude_decimal_degrees, 
-           dados_NA$Longitude_decimal_degrees,
-           dados_NA$Year) 
-summary(dados_NA)
+#somar numero de traits
+traits[,25] <- rowSums(traits[, -1])
 
-#convert lat long to sf
-dados_pontos <- dados_NA %>%
+#separar a soma em novo arquivo
+traits <- traits[,c(1, 25)]
+
+## 4 ADICIONAR NUMERO DE TRAITS EM UMA COLUNA NOVA COM DADOS--------------------
+
+#separar dados que nos vamos usar em um arquivo novo 
+bird_traits <- birds[,c(1, 5, 6, 9, 59, 60, 76)]
+
+#combinar dados que nos vamos usar com o numero de traits cada linha
+bird_traits <- dplyr::left_join(bird_traits, traits, by = "ID_ABT")
+
+#mudar o nome da coluna para "traits"
+bird_traits <- bird_traits %>% 
+  dplyr::rename( Traits = ...25) 
+bird_traits
+
+##4 FORMATAR DADOS GEOESPACIAS
+
+#transformar nosso arquivo para sf
+dados_pontos <- bird_traits %>%
   sf :: st_as_sf(coords = c("Longitude_decimal_degrees",
                             "Latitude_decimal_degrees"), 
-                            crs = 4326)
-#import biomes
+                 crs = 4326)
+dados_pontos
+
+#importar biomas
 bi_2019 <- geobr::read_biomes(year = 2019, showProgress = FALSE)
 bi_2019
 
-#transform dados from WGS84 to SIRGAS2000
+#importar Brasil
+br <- geobr::read_country(2019)
+br
+
+#transformar dados de WGS84 para SIRGAS2000
 dados_sirgas2000 <- sf::st_transform(dados_pontos, crs = 4674)
 
-#visualize data
-plot(bi_2019$geom)
-plot(dados_sirgas2000$geometry, add = TRUE)
 
-#join dados
+#juntar dados para ver quais pontos são foras da Mata Atlântica 
 points_join_biom <- dados_sirgas2000 %>% 
   sf::st_join(x = ., y = bi_2019)
 points_join_biom
 
-#separate points within Mata Atlantica 
-dados_final <- dplyr::filter(points_join_biom, name_biome == "Mata Atlântica")
-as.numeric(dados_final$Year)
-summary(dados_final)
-plot(dados_final$geometry)
+#filtrar os pontos no dentro da Mata Atlântica 
+dados_final <- dplyr::filter(points_join_biom, 
+                             name_biome == "Mata Atlântica")
 
-#save new dataset!
-write.csv(dados_final, file = here::here("dados", "dados_final.csv"))
-
---------------------------------------------------------------------------------
-#' date: 2021-11-03
-
-#clean up dataset
-#points_join_grid <- points_join_grid %>% 
-#dplyr::select(-(code_biome:year))
-#head(points_join_grid)
+#removar colunas de code_biome e year 
+dados_final <- dados_final %>%
+  dplyr::select(-(code_biome:year))
 
 #criar planilha com só o poligano de Mata Atlântica
 bi_2019_MA <- dplyr::filter(bi_2019, name_biome == "Mata Atlântica")
+bi_2019_MA
 
-#projetar dados e Mata Atlântica
-dados_projetados <- sf::st_transform(dados_final, 
-                                     crs = "+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+#Exportar shp para ArcGIS para corretar CRS
+#sf::st_write(obj = dados_final, dsn = here::here("dados", "dados_final.shp"))
+#sf::st_write(obj = bi_2019_MA, dsn = here::here("dados", "bi_2019_MA.shp"))
 
-MA_projetada <- sf::st_transform(bi_2019_MA, 
-                                 crs = "+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+#colocamos os shp finais nos "dados"
+
+#Importar dados de ArcGIS
+
+#nossos dados
+dados_final_Albers_Sir2000 <- sf::st_read(here::here("dados",
+                                                     "dados_Albers_Sir2000", 
+                                                     "dados_final_Albers_Sir2000.shp"), 
+                                          quiet = TRUE)
+dados_final_Albers_Sir2000
+
+#polígono da Mata Atlântica
+bi_2019_MA_Albers_Sir2000 <- sf::st_read(here::here("dados",
+                                                    "MA_Albers_Sir2000", 
+                                                    "bi_2019_MA_Albers_Sir2000.shp"), 
+                                         quiet = TRUE)
+bi_2019_MA_Albers_Sir2000
+
+#polígono do Brasil
+br_Albers_Sir2000 <- sf::st_read(here::here("dados",
+                                            "BR_Albers_Sir2000", 
+                                            "lm_uf_albers_Sir2000.shp"), 
+                                 quiet = TRUE)
+br_Albers_Sir2000
 
 
-#criar grid
-dados_grid <- sf::st_make_grid(x = dados_projetados, cellsize = 100000, what = "polygons") %>%
+#criar grid 
+dados_grid <- sf::st_make_grid(x = dados_final_Albers_Sir2000,
+                               cellsize = 100000, what = "polygons") %>%
   sf::st_as_sf() %>%
-  dplyr::filter(sf::st_intersects(x = ., y = MA_projetada, sparse = FALSE))
+  dplyr::filter(sf::st_intersects(x = ., y = bi_2019_MA_Albers_Sir2000, 
+                                  sparse = FALSE))
 
 #plot com grid
-plot(dados_projetados$geometry, col = "gray", main = NA, axes = TRUE, graticule = TRUE)
-plot(dados_grid, col = NA, border = "red", lwd = 2, add = TRUE)
+#plot(dados_final_Albers_Sir2000$geometry, col = "gray", main = NA,
+     #axes = TRUE, graticule = TRUE)
+#plot(dados_grid, col = NA, border = "red", lwd = 2, add = TRUE)
 
-#assign id's to grid dataset so we can join with points
-tibble::rowid_to_column(dados_grid, "id")
 
-#add grid dataset information to the points dataset
-points_join_grid <- dados_projetados %>% 
+#atribuir um id para cada quadrícula
+grid_id <- tibble::rowid_to_column(dados_grid, "id")
+
+#juntar informaçâo de grid com nossos dados 
+points_join_grid <- dados_final_Albers_Sir2000 %>% 
   sf::st_join(x = ., y = grid_id)
 points_join_grid
 
-#next steps: determine how many points are in each square and create a 
-#spreadsheet with this information that we can then join back to the grid
-#dataset and plot point density 
-
-#maybe make a binary matrix??
-
--------
-#Alternative method to create a grid (see link below for more information)
-#We were not able to extract which points corresponded to which quadrats, 
-#created a file type that was hard to work with (quadracount)
 
 
-#https://mgimond.github.io/Spatial/point-pattern-analysis-in-r.html
+##5 CALCULAR QUANTOS AMOSTRAS POR QUADRÍCULA------------------------------------ 
 
-#dados_final_ppp <- as.ppp(dados_projetados)
+#renomear arquivo de points_join_grid para manipular ele
+samples <- points_join_grid
 
-#bi_2019_MA_owin <- spatstat.geom::as.owin(MA_projetada)
+#redefinir como um tibble
+samples <- as.tibble(samples)
 
-#dados_final_ppp
-#marks(dados_final_ppp)
-#dados_final_ppp <- rescale(dados_final_ppp, 1000)
-#spatstat.geom::Window(dados_final_ppp) <- bi_2019_MA_owin
+#separar colunas de ID_ABT e id (id de quadrícula)
+samples <- samples[,c(1, 8)]
 
-#plot(dados_final_ppp, main=NULL, cols=rgb(0,0,0,.2), pch=20)
+#contar numero de amostras por quadrícula
+samples <- samples %>% group_by(id) %>% tally()
 
-#Q <- spatstat.geom::quadratcount(dados_final_ppp, nx= 20, ny=20)
+#juntar com grid_id
+grid_samples <- dplyr::left_join(grid_id, samples, by = "id")
+
+#remover as NAs
+grid_samples <-tidyr::replace_na(data = grid_samples, list(n = 0))
 
 
-#plot(dados_final_ppp, pch=20, cols="grey70", main=NULL)  # Plot points
-#plot(Q, add=TRUE) 
+##6 CALCULAR QUANTOS TRAITS POR QUADRÍCULA--------------------------------------
 
-#?spatstat.geom
+#renomear arquivo de points_join_grid para manipular ele
+traits <- points_join_grid
 
-# Compute the density for each quadrat
-#Q.d <- intensity(Q)
-#print(Q)
-#summary(Q)
+#redefinir como um tibble
+traits <- as.tibble(traits)
+
+#separar colunas de traits e id (id de quadrícula)
+traits <- traits[,c(6, 8)]
+
+#contar numero total de traits por quadrícula
+traits <- traits %>%
+  group_by(id) %>%
+  summarise(
+    traits_s = sum(traits)
+  )
+
+#juntar com grid_samples
+samples_traits <- dplyr::left_join(grid_samples, traits, by = "id")
+
+#remover as NAs
+samples_traits <-tidyr::replace_na(data = samples_traits, list(traits_s = 0))
+
+#renomear colunas de n e traits_s
+samples_traits <- samples_traits %>% 
+  dplyr::rename(Amostras = n ,  Traits = traits_s) 
+
+
+##7 DEFINIR O ANO MAIS RECENTE QUE A QUADRÍCULA FOI AMOSTRADO-------------------  
+
+#renomear arquivo de points_join_grid para manipular ele
+year <- points_join_grid
+
+#redefinir como um tibble
+year <- as.tibble(year)
+
+#separar colunas de year e id (id de quadrícula)
+year <- year[,c(1, 5, 8)]
+
+#colocar NA em vez de 0 
+year[year == 0] <- NA
+
+#remover NAs
+year <- tidyr::drop_na(data = year,
+                       any_of("Year"))
+
+#identificar ano mais recente (o mais antigo, colocar "min" em vez
+#de max) 
+year <- year %>%
+  group_by(id) %>%
+  summarise(
+    year = min(Year)
+  )
+
+samples_traits_year <- dplyr::left_join(samples_traits, year, by = "id")
+
+#renomear a coluna de year
+samples_traits_year <- samples_traits_year %>% 
+  dplyr::rename(Ano = year) 
+
+#produto final
+samples_traits_year
+
+##8 CRIAR OS MAPAS DE GRID------------------------------------------------------
+
+#mapa de amostras
+mapa_amostras <- ggplot() +
+  geom_sf(data = br_Albers_Sir2000, fill = "gray")+
+  geom_sf(data = bi_2019_MA_Albers_Sir2000)+
+  geom_sf(data = dados_final_Albers_Sir2000$geometry,)+
+  geom_sf(data = samples_traits_year, aes(fill = Amostras), color = gray(.5))+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .8) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "br", which_north = "true",
+                         pad_x = unit(0, "cm"), pad_y = unit(.5, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  xlab("Longitude") + ylab("Latitude")
+mapa_amostras
+
+#mapa de traits
+mapa_traits <- ggplot() +
+  geom_sf(data = br_Albers_Sir2000, fill = "gray")+
+  geom_sf(data = bi_2019_MA_Albers_Sir2000)+
+  geom_sf(data = dados_final_Albers_Sir2000$geometry,)+
+  geom_sf(data = samples_traits_year, aes(fill = Traits), color = gray(.5))+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .8) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "br", which_north = "true",
+                         pad_x = unit(0, "cm"), pad_y = unit(.5, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  xlab("Longitude") + ylab("Latitude")
+mapa_traits
+
+#mapa de ano
+mapa_ano <- ggplot() +
+  geom_sf(data = br_Albers_Sir2000, fill = "gray")+
+  geom_sf(data = bi_2019_MA_Albers_Sir2000)+
+  geom_sf(data = dados_final_Albers_Sir2000$geometry,)+
+  geom_sf(data = samples_traits_year, aes(fill = Ano), color = gray(.5))+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .8) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "br", which_north = "true",
+                         pad_x = unit(0, "cm"), pad_y = unit(.5, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  xlab("Longitude") + ylab("Latitude")
+mapa_ano
+
+##9 CRIAR O MAPA DE KERNEL------------------------------------------------------
+
+# Cria uma janela raster
+bi_2019_MA_Albers_Sir2000_Owin <- as.owin(bi_2019_MA_Albers_Sir2000)
+
+# Converte os pontos em ppp (raster spatstat)
+dados_Albers_ppp <- as.ppp(dados_final_Albers_Sir2000)
+
+#Limita por janela
+Window(dados_Albers_ppp) <- bi_2019_MA_Albers_Sir2000_Owin
+
+# Conversão de medida
+dados_Albers_ppp_km <- rescale(dados_Albers_ppp, 1000, "km")
+
+#Contagem de Ponto por Quadrícula
+Q <- spatstat.geom::quadratcount(dados_Albers_ppp_km, nx=8, ny=8)
+plot(Q)
+
+#Intensidade por quadrícula
+Q.d <- intensity(Q)
+plot(intensity(Q, image = TRUE))
+
+#kernell
+kernel <- density(dados_Albers_ppp_km)
+
+#plot
+plot(kernel)
+
+#tem que exportar a imagem do kernel no Plot window
+
+
+##10 ANÁLISES ESTATÍSTICA-------------------------------------------------------
+
+#carregar dados
+dados_est <- readr::read_csv(here::here("dados", "dados_est.csv"),
+                         locale = readr::locale(encoding = "latin1"))
+
+#histograma
+traits_hist <- ggplot(data = dados_est,
+      aes(x = Nu_Amostras, fill = Traits)) +
+      geom_histogram(bins = 5, alpha = 20)+
+      labs(x="Número de Amostras",y= "frequência", title = "NÚMERO DE TRAITS")+
+      theme_bw(base_size = 10)+
+      geom_density()+
+      theme(plot.title = element_text(hjust = 0.5))
+traits_hist
+
+
+#Probabilidade que um trait vai ser medida 
+
+# virando linha em coluna
+gather<-gather(dados_est)
+
+#criando colunas
+dados_est["total"]<-c("72484") 
+
+#definir coluna de total como integer
+dados_est$total <- as.integer(dados_est$total)
+
+#cacular a probabilidade 
+dados_est["prob_trait"] <- (dados_est["Nu_Amostras"]/dados_est["total"])*100
+dados_est
+
+
+##11 EXPORTAR OS MAPAS----------------------------------------------------------
+
+ggsave(
+  filename = here::here("resultados", "mapa_amostras.png"),
+  plot = mapa_amostras, 
+  width = 20, 
+  height = 20, 
+  units = "cm", 
+  dpi = 720
+)
+
+ggsave(
+  filename = here::here("resultados", "mapa_traits.png"),
+  plot = mapa_traits, 
+  width = 20, 
+  height = 20, 
+  units = "cm", 
+  dpi = 720
+)
+
+ggsave(
+  filename = here::here("resultados", "mapa_ano_min.png"),
+  plot = mapa_ano, 
+  width = 20, 
+  height = 20, 
+  units = "cm", 
+  dpi = 720
+)
+
+ggsave(
+  filename = here::here("resultados", "traits_hist.png"),
+  plot = traits_hist, 
+  width = 20, 
+  height = 20, 
+  units = "cm", 
+  dpi = 720
+)
+
+
+
+
